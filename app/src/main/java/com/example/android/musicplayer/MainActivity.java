7a84fca99494f376d8444a467129cd5f48dc3b26
Roy Mustang <roy.mustang.net@gmail.com>
2018-04-20 07:55:05 +0800

    added all songs

--- app/src/main/java/com/example/android/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/android/musicplayer/MainActivity.java
@@ -14,9 +14,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ArrayList<MusicItem> musics = new ArrayList<MusicItem>();
-        String[] musicnames = {"今生情不变","海阔天空","红豆","女驸马","青青河边草","生活不止眼前的苟且"};
-        String[] singers = {"林志豪","Beyond","王菲","严凤英","高胜美","许巍"};
-        String[] albums = {"乐作剧","海阔天空","Eyes on me","黄梅戏珍藏","电视剧原声","生活不止眼前的苟且"};
+        String[] musicnames = {"今生情不变","海阔天空","红豆","女驸马","青青河边草","生活不止眼前的苟且","爱的路上只有我和你","大海","大约在冬季"};
+        String[] singers = {"林志豪","Beyond","王菲","严凤英","高胜美","许巍","任贤齐","张雨生","齐秦"};
+        String[] albums = {"乐作剧","海阔天空","Eyes on me","黄梅戏珍藏","电视剧原声","生活不止眼前的苟且","飞鸟","大海","柔情主义"};
 //        musics.add(new MusicItem(""))
         for(int i = 0; i < musicnames.length; i++){
             musics.add(new MusicItem(musicnames[i],
