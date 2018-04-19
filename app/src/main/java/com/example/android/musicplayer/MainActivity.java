af8cfbb35855670736d61ee07b4b99037fbc1d52
Roy Mustang <roy.mustang.net@gmail.com>
2018-04-20 00:31:05 +0800

    add display all songs

--- app/src/main/java/com/example/android/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/android/musicplayer/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.android.musicplayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<MusicItem> musics = new ArrayList<MusicItem>();
+        String[] musicnames = {};
+        String[] singers = {};
+        String[] albums = {};
+//        musics.add(new MusicItem(""))
+        for(int i = 0; i < musicnames.length; i++){
+            musics.add(new MusicItem(musicnames[i],
+                    singers[i],
+                    albums[i]));
+        }
+
+        MusicItemsAdapter adapter = new MusicItemsAdapter(this,musics);
+        ListView listview = (ListView) findViewById(R.id.list_view);
+        listview.setAdapter(adapter);
     }
 }
