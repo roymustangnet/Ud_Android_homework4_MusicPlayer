8086c7aebb83f9266485d4d96374fd7e5c33fb88
Roy Mustang <roy.mustang.net@gmail.com>
2018-04-20 00:41:04 +0800

    add some data

--- app/src/main/java/com/example/android/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/android/musicplayer/MainActivity.java
@@ -14,9 +14,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ArrayList<MusicItem> musics = new ArrayList<MusicItem>();
-        String[] musicnames = {};
-        String[] singers = {};
-        String[] albums = {};
+        String[] musicnames = {"今生情不变","海阔天空","红豆","女驸马","青青河边草","生活不止眼前的苟且"};
+        String[] singers = {"林志豪","Beyond","王菲","严凤英","高胜美","许巍"};
+        String[] albums = {"乐作剧","海阔天空","Eyes on me","黄梅戏珍藏","电视剧原声","生活不止眼前的苟且"};
 //        musics.add(new MusicItem(""))
         for(int i = 0; i < musicnames.length; i++){
             musics.add(new MusicItem(musicnames[i],
